6899adcba9be3cd14e4be7746c0b0c665feab395
bruninha98 <129185317+doardasantos98@users.noreply.github.com>
2024-08-27 08:25:34 -0300

    jjgjjghjgfhg

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPergunta = document.querySelector(".caixa-perguntas");
 const caixaAlternativa = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const caixaResultado = document.querySelector(".")
+const textoResultado = document.querySelector(".texto-resultado");
